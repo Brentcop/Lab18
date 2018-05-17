068033426348ffae8f71c3870a6590252fd281d6
brentcop <coppinger.brent@student.oaklandcc.edu>
2018-05-17 08:52:16 -0400

    First commit

---
+++ src/MainApp.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,2,2,4,5,5,7,8,4,4,1,0,10};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		LinkedList.countOccur(arr);
+		//System.out.println(Arrays.toString(arr));
+	
+	}
+
+}
